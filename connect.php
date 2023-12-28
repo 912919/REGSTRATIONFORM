5d8865c40339e6f57886927271170d751b2a455f
912919 <110796967+912919@users.noreply.github.com>
2023-12-28 19:05:42 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,27 @@
+
+
+
+<?php
+    $id = $_POST['id'];
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	$conn = new mysqli('localhost','root','Aashu123@','mydb123');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into customer123(id, firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isssssi", $id, $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
+
